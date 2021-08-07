4aa231dbe9b1a506370d898bc71e6cc1d3c18814
Mario Rivero <mariorivero051@gmail.com>
2021-08-07 19:17:56 +0000

    Added captions to speech recognition

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+from audio import printWAV
+import time, random, threading
+from turbo_flask import Turbo
 
 # gets the name of the file so Flask knows it's name
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6089e82b1086f18902ed80012922fb75'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+interval=10
+FILE_NAME = "Ryuk.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -28,11 +34,45 @@ def home():
 def about():
   return render_template('about.html', subtitle='About Page', text='Learn more about me!')
 
+
 @app.route("/captions")
 def captions():
   TITLE = "Yee"
-  FILE_NAME = "Yee.wav"
   return render_template("captions.html", songName=TITLE, file=FILE_NAME)
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions, daemon=True).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
